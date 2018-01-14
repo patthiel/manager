436f49bce80ef2926d5dd30074f60993295b8dec
Pat Thiel <pthiel@urbn.com>
2018-01-14 18:01:37 -0500

    stub out more forgot password tests

--- src/specs/login/forgot-pass.spec.spec.js
+++ src/specs/login/forgot-pass.spec.spec.js
@@ -1,4 +1,3 @@
-import { env } from 'process';
 import { assertGenericErrorDisplays } from '../utils/login-utils';
 
 import {
@@ -6,10 +5,27 @@ import {
   forgotPasswordForm,
 } from '../ui-maps/login-ui-map';
 
-xdescribe('Linode Manager - Login - Forgot Password Suite', () => {
+describe('Linode Manager - Login - Forgot Password Suite', () => {
   beforeAll(() => browser.url('/'));
 
   it('should display the forgot password page', () => {
     browser.click(loginForm.forgot);
+    const forgotPasswordElems = Object.values(forgotPasswordForm);
+
+    forgotPasswordElems.forEach(e => {
+      expect($(e).isVisible()).toBe(true);
+    });
+  });
+
+  xit('should display a error dialog when submitting without username', () => {
+
+  });
+
+  xit('should throw an error when entering an email', () => {
+
+  });
+
+  xit('should display success message on submision of form', () => {
+
   });
 });
