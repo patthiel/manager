b1f696a3532ca12ff0763ea6403e98e2f650769e
Patrick Thiel <pthiel@linode.com>
2018-05-15 10:26:09 -0400

    snapshot comparison WIP

---
+++ storyshot.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "collectCoverageFrom": [
+      "src/**/*.{js,jsx,ts,tsx}"
+    ],
+    "setupFiles": [
+      "<rootDir>/config/polyfills.js"
+    ],
+    "setupTestFrameworkScriptFile": "<rootDir>/config/testSetup.js",
+    "testRegex": "\/e2e\/.*.tsx",
+    "testEnvironment": "node",
+    "testURL": "http://localhost",
+    "transform": {
+      "^.+\\.tsx?$": "<rootDir>/config/jest/typescriptTransform.js",
+      "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
+      "^(?!.*\\.(js|jsx|mjs|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
+    },
+    "transformIgnorePatterns": [
+      "[/\\\\](node_modules|src)[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$"
+    ],
+    "moduleNameMapper": {
+      "\\.svg$": "<rootDir>/src/components/NullComponent",
+      "^react-native$": "react-native-web",
+      "^src/(.*)": "<rootDir>/src/$1"
+    },
+    "moduleFileExtensions": [
+      "mjs",
+      "web.ts",
+      "ts",
+      "web.tsx",
+      "tsx",
+      "web.js",
+      "js",
+      "web.jsx",
+      "jsx",
+      "json",
+      "node"
+    ],
+    "globals": {
+      "ts-jest": {
+        "tsConfigFile": "tsconfig.test.json"
+      }
+    }
+}
