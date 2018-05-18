e60dd0685b23e9227d07fbc7b744ae11a4b099ff
Patrick Thiel <pthiel@linode.com>
2018-05-18 12:19:15 -0400

    Move snapshot stories into src/components

---
+++ src/components/Storyshots.test.tsx
@@ -0,0 +1,10 @@
+import initStoryshots, { imageSnapshot } from '@storybook/addon-storyshots';
+
+const getMatchOptions = ({context : {kind, story}, url}) => {
+  return {
+    failureThreshold: 0.5,
+    failureThresholdType: 'percent',
+  }
+}
+
+initStoryshots({suite: 'Image storyshots', test: imageSnapshot({getMatchOptions})});
