f51e701efe03e110d3dcd7299cbc12664ee746f0
Patrick Thiel <pthiel@linode.com>
2018-05-18 12:23:07 -0400

    Fix syntax errors

--- src/components/Storyshots.test.tsx
+++ src/components/Storyshots.test.tsx
@@ -1,10 +1,10 @@
 import initStoryshots, { imageSnapshot } from '@storybook/addon-storyshots';
 
-const getMatchOptions = ({context : {kind, story}, url}) => {
+const getMatchOptions = ( {context : { kind, story }, url } ) => {
   return {
     failureThreshold: 0.5,
     failureThresholdType: 'percent',
-  }
-}
+  };
+};
 
-initStoryshots({suite: 'Image storyshots', test: imageSnapshot({getMatchOptions})});
+initStoryshots({ suite: 'Image storyshots', test: imageSnapshot({ getMatchOptions }) });
